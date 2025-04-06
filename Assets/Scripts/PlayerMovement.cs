8bb68f35d61c4139db93444903d9a51a91729f74
RICARDOS-LAPTOP\Ricardo Malavé <ricardomalaveo@gmail.com>
2025-04-06 23:44:43 +0200

    last checks

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using JetBrains.Annotations;
 using System.Collections;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
-using TreeEditor;
+
 
 public class Respawn : MonoBehaviour
 {
